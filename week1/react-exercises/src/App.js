d68c74294eb260c0b90520eb65e4e97f9f87a17e
Marwa <makw90@hotmail.com>
2021-05-25 23:02:27 +0200

    React week 1

---
+++ week1/react-exercises/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import HobbyList from "./ex1/HobbiesList";
+import GuaranteeItem from "./ex2/guaranteeList";
+import Counter from "./ex3/counter";
+
+function App() {
+  return (
+    <div className="App">
+      <HobbyList />
+      <GuaranteeItem />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
