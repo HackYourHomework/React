9737666c24a1cf12fa9ba1dfbf4b0058c2cc3ab6
Maher <ma.meer@hotmail.com>
2021-05-30 21:32:57 +0200

    Finish home work after sunday session

--- week2/react-exercises/src/App.js
+++ week2/react-exercises/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Header from "./components/Header";
 import Friend from "./components/ex1/Friend";
 import Dog from "./components/ex2/DogGallery";
 import RandomJoke from "./components/ex3/RandomJoke";
@@ -8,15 +7,15 @@ const App = () => {
   return (
     <>
       <div className="container">
-        <Header title="New friend on demand" />
+        <h2>New friend on demand</h2>
         <Friend />
       </div>
       <div className="container">
-        <Header title="Dog photo gallery" />
+        <h2>Dog photo gallery</h2>
         <Dog />
       </div>
       <div className="container">
-        <Header title="Random Joke Generator" />
+        <h2>Random Joke Generator</h2>
         <RandomJoke />
       </div>
     </>
--- week2/react-exercises/src/components/Header.js
+++
@@ -1,5 +0,0 @@
-const Header = ({ title }) => {
-  return <h2>{title}</h2>;
-};
-
-export default Header;
