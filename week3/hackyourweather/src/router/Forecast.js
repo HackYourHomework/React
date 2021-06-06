51dbe3f70dfa67a3ea63a08d7a069a208068e2d7
Maher <ma.meer@hotmail.com>
2021-06-06 20:59:17 +0200

    adding context

--- week3/hackyourweather/src/router/Forecast.js
+++ week3/hackyourweather/src/router/Forecast.js
@@ -1,37 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Link, useParams, useHistory } from "react-router-dom";
 import Chart from "../components/Chart";
+import { useGlobalContext } from "../components/context";
 
 const Forecast = () => {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [cityForecast, setCityForecast] = useState([]);
-  const [cityName, setCityName] = useState("");
+  const { notification, cityName, cityForecast, getCityForecast } =
+    useGlobalContext();
   const { cityId } = useParams();
   const history = useHistory();
 
   useEffect(() => {
-    (async () => {
-      try {
-        setLoading(true);
-        const resp = await fetch(
-          `https://api.openweathermap.org/data/2.5/forecast?id=${cityId}&APPID=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}&units=metric`
-        );
-        const data = await resp.json();
-        if (!resp.ok) {
-          throw new Error(data.message);
-        }
-        setLoading(false);
-        setError(false);
-        setCityName(data.city.name);
-        setCityForecast(data.list);
-      } catch (err) {
-        setLoading(false);
-        setError(err.message);
-        setCityForecast();
-      }
-    })();
+    getCityForecast(cityId);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cityId]);
+
   return (
     <div className="forecast">
       <h1>Forecast</h1>
@@ -40,8 +22,7 @@ const Forecast = () => {
           Weather
         </button>
       </Link>
-      {error && <h2>{error}</h2>}
-      {loading && <h2>loading ...</h2>}
+      {notification && <h2>{notification}</h2>}
       {cityName && <h2>{cityName}</h2>}
       {cityForecast && <Chart cityForecast={cityForecast} />}
     </div>
--- week3/hackyourweather/src/router/Home.js
+++ week3/hackyourweather/src/router/Home.js
@@ -1,48 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import City from "../components/City";
+import { useGlobalContext } from "../components/context";
 import Search from "../components/Search";
 
 const Cities = () => {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [citiesWeatherMap, setCitiesWeatherMap] = useState([]);
-
-  const onDelete = (id) => {
-    setCitiesWeatherMap(citiesWeatherMap.filter((city) => city.id !== id));
-  };
-
-  const getCityWeather = async (cityName) => {
-    try {
-      if (cityName) {
-        setLoading(true);
-        const resp = await fetch(
-          `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}&units=metric`
-        );
-        const data = await resp.json();
-        if (!resp.ok) {
-          throw new Error(data.message);
-        }
-        setLoading(false);
-        setError(false);
-        if (!citiesWeatherMap.some((city) => city.id === data.id)) {
-          setCitiesWeatherMap([data, ...citiesWeatherMap]);
-        }
-      } else {
-        setError(`empty input`);
-      }
-    } catch (err) {
-      setLoading(false);
-      setError(err.message);
-      setCitiesWeatherMap([...citiesWeatherMap]);
-    }
-  };
+  const { notification, citiesWeatherMap, onDelete, getCityWeather } =
+    useGlobalContext();
 
   return (
     <>
       <h1>Weather</h1>
       <Search getCityWeather={getCityWeather} />
-      {error && <h2>{error}</h2>}
-      {loading && <h2>loading ...</h2>}
+      {notification && <h2>{notification}</h2>}
       {citiesWeatherMap &&
         citiesWeatherMap.map((cityWeatherMap) => (
           <City
