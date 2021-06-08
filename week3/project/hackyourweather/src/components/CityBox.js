c4914921927e188e3daac023ffc5aba8fdf0c743
Mahmood <pejvakco@gmail.com>
2021-06-08 19:53:57 +0200

    add project

---
+++ week3/project/hackyourweather/src/components/CityBox.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import City from './City';
+import Location from './Location';
+import Temperature from './Temperature';
+import Description from './Description';
+
+const CityBox = ({ cityData }) => {
+  return (
+    <div
+      style={{
+        textAlign: 'left',
+        border: '1px solid black',
+        width: '500px',
+        padding: '20px',
+        margin: '0 auto',
+      }}
+    >
+      <City cityData={cityData} />
+      <Description cityData={cityData} />
+      <Temperature cityData={cityData} />
+      <Location cityData={cityData} />
+    </div>
+  );
+};
+
+export default CityBox;
---
+++ week3/project/hackyourweather/src/components/Temperature.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Temperature = ({ cityData }) => {
+  const { temp_min, temp_max } = cityData.main;
+  const minCelsius = temp_min - 273.15;
+  const maxCelsius = temp_max - 273.15;
+  return (
+    <div>
+      <p>Minimum Temperature: {minCelsius.toFixed(0)}</p>
+      <p>Maximum Temperature: {maxCelsius.toFixed(0)}</p>
+    </div>
+  );
+};
+
+export default Temperature;
